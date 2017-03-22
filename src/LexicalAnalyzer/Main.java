9807fe3c48362f0f4db7cb35107f5360ec522574
cVladu <ktalex993@gmail.com>
2017-03-22 14:47:52 +0200

    Modified InputClass

--- src/LexicalAnalyzer/Main.java
+++ src/LexicalAnalyzer/Main.java
@@ -1,18 +1,15 @@
 package LexicalAnalyzer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
 	
 	private static String inputFile = "C:\\Users\\Cata\\Desktop\\test.c";
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		InputClass test = new InputClass(inputFile);
-		try {
-			System.out.println(test.readFile());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		ArrayList<String> tmpList = new ArrayList<String>();
+		tmpList = test.getArrayAsList();
+		System.out.println(tmpList);
 	}
-
 }
